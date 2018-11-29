7299f38ce78d04bb7e5bf07f67d549f966114ae5
Karan Pahlani <pahlani@gmail.com>
2018-11-29 03:32:54 -0600

    first commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php include "templates/header.php"; ?>
+
+<ul>
+	<li><a href="create.php"><strong>Add New User</strong></a></li>
+	<li><a href="read.php"><strong>Find a User</strong></a></li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
