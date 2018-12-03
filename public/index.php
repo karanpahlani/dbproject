c75dacc867b0dedbfdd110e1a54fe01b729dad68
Karan Pahlani <pahlani@gmail.com>
2018-12-02 20:08:56 -0600

    changes

--- public/index.php
+++ public/index.php
@@ -1,14 +1,25 @@
 <?php include "templates/header.php"; ?>
-
+    <link rel="stylesheet" href="css/style.css">
+<h1>DASHBOARD</h1>
+    <h2>USERS</h2>
 <ul>
+
 	<li><a href="create.php"><strong>Add New User</strong></a></li>
 	<li><a href="read.php"><strong>Find a User</strong></a></li>
 
-    <li><a href="addDonation.php"><strong>Add a Donation</strong></a></li>
-    <li><a href="addItem.php"><strong>Add a Item Type</strong></a></li>
 
-    <a href="readDonations.php">View Donations</a>
+    <li><a href="addItem.php"><strong>Add a Item Type</strong></a></li>
 
 </ul>
 
+
+    <h2>Requests</h2>
+    <li><a href="matcher.php"><strong>Request for Aid</strong></a></li>
+
+
+    <h2>Donations</h2>
+<ul>
+    <li><a href="addDonation.php"><strong>Add a Donation</strong></a></li>
+   <li> <a href="readDonations.php">View Donations</a> </li>
+</ul>
 <?php include "templates/footer.php"; ?>
---
+++ public/matcher.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: karan
+ * Date: 2018-12-02
+ * Time: 20:07
+ */
