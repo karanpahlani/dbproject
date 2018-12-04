bc90106b95ea83950bb97688e9a7e32b087a533d
Karan Pahlani <pahlani@gmail.com>
2018-12-04 02:11:19 -0600

    view fix

--- public/index.php
+++ public/index.php
@@ -6,17 +6,17 @@
 
 	<li><a href="create.php"><strong>Add New User</strong></a></li>
 	<li><a href="read.php"><strong>Find a User</strong></a></li>
-
-
-    <li><a href="addItem.php"><strong>Add a Item Type</strong></a></li>
-
-    <li><a href="readItem.php"><strong>List of Items</strong></a></li>
 </ul>
 
 
-    <h2>Requests</h2>
-    <li><a href="matcher.php"><strong>Request for Aid</strong></a></li>
+    <h2>Items</h2>
+<ul>
+    <li><a href="addItem.php"><strong>Add a Item Type</strong></a></li>
+    <li><a href="readItem.php"><strong>List of Items</strong></a></li>
+</ul>
 
+<h2>Requests</h2>
+<li><a href="matcher.php"><strong>Request for Aid</strong></a></li>
 
     <h2>Donations</h2>
 <ul>
